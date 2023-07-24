16d55fa411632aa60c75a3ef8fc45177981b39bd
TejaswiJadhav <104181258+Tejabhai2002@users.noreply.github.com>
2023-07-24 21:21:51 +0530

    Add files via upload

---
+++ slider_carousel.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'WI  vs IND 357/4 (50)',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'Loki',
+        des: 'Premise. After stealing the Tesseract during the events of Avengers: Endgame (2019), an alternate version of Loki is brought to the mysterious Time Variance Authority (TVA), a bureaucratic organization that exists outside of time and space and monitors the timeline.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'Wanda Vision',
+        des: 'It follows Wanda Maximoff and Vision as they live an idyllic suburban life in the town of Westview, New Jersey, until their reality starts moving through different decades of sitcom homages and television tropes. Schaeffer served as head writer for the series, which was directed by Matt Shakman.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. However, when sinister monsters known as the Druun threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, those same monsters have returned, and it is up to a lone warrior to track down the last dragon and stop the Druun for good.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'an Italian teen sea monster named Luca (Jacob Tremblay) who runs away to the surface world with his bestie and fellow teen sea monster Alberto ',
+        image: 'images/slider 5.PNG'
+    }
+]
